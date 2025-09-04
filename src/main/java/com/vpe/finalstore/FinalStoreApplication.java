39d5caea0a064383487cf5d73ea48520f742e629
venelinpetrov <vpetrov33tu@gmail.com>
2025-09-04 16:07:51 +0300

    Initial setup

---
+++ src/main/java/com/vpe/finalstore/FinalStoreApplication.java
@@ -0,0 +1,14 @@
+package com.vpe.finalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalStoreApplication.class, args);
+        System.out.println("hi");
+    }
+
+}
