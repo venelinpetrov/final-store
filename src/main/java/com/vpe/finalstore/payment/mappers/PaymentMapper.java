300070623b32ee75daf6a54ac7f43a04b1899705
Venelin Petrov <venelin.petrov@redis.com>
2026-03-07 17:24:37 +0200

    Refactor payment mapper

--- src/main/java/com/vpe/finalstore/payment/mappers/PaymentMapper.java
+++ src/main/java/com/vpe/finalstore/payment/mappers/PaymentMapper.java
@@ -2,36 +2,18 @@ package com.vpe.finalstore.payment.mappers;
 
 import com.vpe.finalstore.payment.dtos.PaymentDto;
 import com.vpe.finalstore.payment.entities.Payment;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Component
-public class PaymentMapper {
-    public PaymentDto toDto(Payment payment) {
-        if (payment == null) {
-            return null;
-        }
+@Mapper(componentModel = "spring")
+public interface PaymentMapper {
+    @Mapping(target = "invoiceId", source = "invoice.invoiceId")
+    @Mapping(target = "methodName", source = "method.name")
+    @Mapping(target = "status", source = "status.name")
+    PaymentDto toDto(Payment payment);
 
-        var dto = new PaymentDto();
-        dto.setPaymentId(payment.getPaymentId());
-        dto.setInvoiceId(payment.getInvoice().getInvoiceId());
-        dto.setAmount(payment.getAmount());
-        dto.setPaymentDate(payment.getPaymentDate());
-        dto.setMethodName(payment.getMethod() != null ? payment.getMethod().getName() : null);
-        dto.setStatus(payment.getStatus().getName());
-        dto.setStripePaymentIntentId(payment.getStripePaymentIntentId());
-        dto.setStripeChargeId(payment.getStripeChargeId());
-        dto.setStripeCustomerId(payment.getStripeCustomerId());
-
-        return dto;
-    }
-
-    public List<PaymentDto> toDto(List<Payment> payments) {
-        return payments.stream()
-            .map(this::toDto)
-            .collect(Collectors.toList());
-    }
+    List<PaymentDto> toDto(List<Payment> payments);
 }
 
