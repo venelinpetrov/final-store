360749add9968892899a1cd62d639c387e426c6f
Venelin Petrov <venelin.petrov@redis.com>
2026-03-22 22:30:45 +0200

    Minor improvement

--- src/main/java/com/vpe/finalstore/FinalStoreApplication.java
+++ src/main/java/com/vpe/finalstore/FinalStoreApplication.java
@@ -8,7 +8,7 @@ public class FinalStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FinalStoreApplication.class, args);
-        System.out.println("hi");
+        System.out.println("Server is running!\nVisit http://localhost:8080/swagger-ui/index.html to view the API documentation.");
     }
 
 }
