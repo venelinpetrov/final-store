b185d72bcebed4e65e14aca9b579e405fa9d0cd8
Venelin Petrov <venelin.petrov@redis.com>
2026-03-29 16:53:24 +0300

    [Shipments] Update carrier endpoint

--- src/main/java/com/vpe/finalstore/shipment/controllers/CarrierController.java
+++ src/main/java/com/vpe/finalstore/shipment/controllers/CarrierController.java
@@ -2,17 +2,20 @@ package com.vpe.finalstore.shipment.controllers;
 
 import com.vpe.finalstore.shipment.dtos.CarrierCreateDto;
 import com.vpe.finalstore.shipment.dtos.CarrierDto;
+import com.vpe.finalstore.shipment.dtos.CarrierUpdateDto;
 import com.vpe.finalstore.shipment.mappers.CarrierMapper;
 import com.vpe.finalstore.shipment.services.CarrierService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -33,9 +36,22 @@ public class CarrierController {
         return ResponseEntity.ok(carrierMapper.toDto(carriers));
     }
 
+    @Operation(
+        summary = "Create a new carrier"
+    )
     @PostMapping
     public ResponseEntity<CarrierDto> createCarrier(@RequestBody CarrierCreateDto dto) {
         var carrier = carrierService.create(dto);
+
+        return ResponseEntity.ok(carrierMapper.toDto(carrier));
+    }
+
+    @Operation(
+        summary = "Update a carrier"
+    )
+    @PutMapping("/{carrierId}")
+    public ResponseEntity<CarrierDto> updateCarrier(@PathVariable Integer carrierId, @RequestBody CarrierUpdateDto dto) {
+        var carrier = carrierService.update(carrierId, dto);
 
         return ResponseEntity.ok(carrierMapper.toDto(carrier));
     }
--- src/main/java/com/vpe/finalstore/shipment/services/CarrierService.java
+++ src/main/java/com/vpe/finalstore/shipment/services/CarrierService.java
@@ -1,6 +1,8 @@
 package com.vpe.finalstore.shipment.services;
 
+import com.vpe.finalstore.exceptions.NotFoundException;
 import com.vpe.finalstore.shipment.dtos.CarrierCreateDto;
+import com.vpe.finalstore.shipment.dtos.CarrierUpdateDto;
 import com.vpe.finalstore.shipment.entities.Carrier;
 import com.vpe.finalstore.shipment.mappers.CarrierMapper;
 import com.vpe.finalstore.shipment.repositories.CarrierRepository;
@@ -19,7 +21,19 @@ public class CarrierService {
         return carrierRepository.findAll();
     }
 
-    public Carrier create(CarrierCreateDto carrier) {
-        return carrierRepository.save(carrierMapper.toEntity(carrier));
+    public Carrier create(CarrierCreateDto dto) {
+        return carrierRepository.save(carrierMapper.toEntity(dto));
+    }
+
+    public Carrier update(Integer carrierId, CarrierUpdateDto dto) {
+        var carrier = carrierRepository.findById(carrierId)
+            .orElseThrow(() -> new NotFoundException("Carrier not found"));
+
+        carrier.setName(dto.getName());
+        carrier.setCode(dto.getCode());
+        carrier.setTrackingUrlTemplate(dto.getTrackingUrlTemplate());
+        carrier.setApiEndpoint(dto.getApiEndpoint());
+
+        return carrierRepository.save(carrier);
     }
 }
