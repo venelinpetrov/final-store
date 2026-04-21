a9483bf0bd1cb48be3955913ad003707f6046a8d
Venelin Petrov <venelin.petrov@redis.com>
2026-04-21 18:46:29 +0300

    [Discounts] Security rules

--- src/main/java/com/vpe/finalstore/discount/security/DiscountSecurityRules.java
+++ src/main/java/com/vpe/finalstore/discount/security/DiscountSecurityRules.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.stereotype.Component;
+import static com.vpe.finalstore.users.enums.RoleEnum.ADMIN;
 
 @Component
 public class DiscountSecurityRules implements SecurityRules {
@@ -14,6 +15,6 @@ public class DiscountSecurityRules implements SecurityRules {
         AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry
     ) {
         registry
-            .requestMatchers(HttpMethod.POST, "/api/discounts").authenticated();
+            .requestMatchers(HttpMethod.POST, "/api/discounts").hasAuthority(ADMIN.authority());
     }
 }
