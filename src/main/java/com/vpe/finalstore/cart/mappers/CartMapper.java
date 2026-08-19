c8c1891beaa24a81b85414e7ee81cfd02287cccf
venelinpetrov <vpetrov33tu@gmail.com>
2026-08-19 18:39:29 +0300

    Cart items are missing some props in the response

--- src/main/java/com/vpe/finalstore/cart/mappers/CartMapper.java
+++ src/main/java/com/vpe/finalstore/cart/mappers/CartMapper.java
@@ -4,10 +4,15 @@ import com.vpe.finalstore.cart.dtos.CartDto;
 import com.vpe.finalstore.cart.dtos.CartItemDto;
 import com.vpe.finalstore.cart.entities.Cart;
 import com.vpe.finalstore.cart.entities.CartItem;
+import com.vpe.finalstore.product.mappers.ProductVariantMapper;
+
 import org.mapstruct.Mapper;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    uses = ProductVariantMapper.class
+)
 public interface CartMapper {
     CartDto toDto(Cart cart);
 
