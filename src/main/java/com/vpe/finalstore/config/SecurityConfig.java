c1ced80b6fb3e45557aa3238fdfda6e32d2762cb
venelinpetrov <vpetrov33tu@gmail.com>
2026-09-09 10:25:29 +0300

    Whitelist port 4173 in CORS config

--- src/main/java/com/vpe/finalstore/config/SecurityConfig.java
+++ src/main/java/com/vpe/finalstore/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4173"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
